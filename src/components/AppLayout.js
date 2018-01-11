d237cd1662398bf38c96ab37022743ae7293035a
kyle <kdeak1210@gmail.com>
2018-01-11 07:31:17 -0500

    Mapping out the UI with some dummy data

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -4,5 +4,5 @@ export default styled.div`
   display: grid;
   height: 100vh;
   grid-template-columns: 100px 250px 1fr;
-  grid-template-rows: quto 1fr auto;
+  grid-template-rows: auto 1fr auto;
 `;
