e9c0f04266fba91d221d645e1957ef463f141ff2
kyle <kdeak1210@gmail.com>
2018-01-18 21:11:45 -0500

    Update allTeamsQuery to also do an 'inviteTeams' query
    inviteTeams gets the teams that the current user has been invited to. We'll then join the allTeams and inviteTeams arrays w/ spread

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -10,18 +10,23 @@ import SendMessage from '../components/SendMessage';
 import Sidebar from '../containers/Sidebar';
 import { allTeamsQuery } from '../graphql/team';
 
-const ViewTeam = ({ data: { loading, allTeams }, match: { params: { teamId, channelId } } }) => {
+const ViewTeam = ({
+  data: { loading, allTeams, inviteTeams },
+  match: { params: { teamId, channelId } },
+}) => {
   if (loading) {
     return null;
   }
 
-  if (!allTeams.length) {
+  const teams = [...allTeams, ...inviteTeams];
+
+  if (!teams.length) {
     return <Redirect to="/create-team" />;
   }
 
   const teamIdInteger = parseInt(teamId, 10);
-  const teamIndex = teamIdInteger ? findIndex(allTeams, ['id', teamIdInteger]) : 0;
-  const team = teamIndex === -1 ? allTeams[0] : allTeams[teamIndex];
+  const teamIndex = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
+  const team = teamIndex === -1 ? teams[0] : teams[teamIndex];
 
   const channelIdInteger = parseInt(channelId, 10);
   const channelIndex = channelIdInteger ? findIndex(team.channels, ['id', channelIdInteger]) : 0;
@@ -30,7 +35,7 @@ const ViewTeam = ({ data: { loading, allTeams }, match: { params: { teamId, chan
   return (
     <AppLayout>
       <Sidebar
-        teams={allTeams.map(t => ({
+        teams={teams.map(t => ({
           id: t.id,
           letter: t.name.charAt(0).toUpperCase(),
         }))}
