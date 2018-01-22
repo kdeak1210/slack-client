8a52dca00e73b22cbc9941a86ca74c14ba28807e
kyle <kdeak1210@gmail.com>
2018-01-22 09:20:49 -0500

    Using semantic-ui Comment components to render out the messages that are loaded in by the MessageContainer

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Comment } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Messages from '../components/Messages';
 
+const Message = ({
+  id, text, created_at, user: { username },
+}) => (
+  <Comment key={`message-${id}`}>
+    <Comment.Avatar src="/assets/images/avatar/small/matt.jpg" />
+    <Comment.Content>
+      <Comment.Author as="a">{ username }</Comment.Author>
+      <Comment.Metadata>
+        <div>{ created_at }</div>
+      </Comment.Metadata>
+      <Comment.Text>{ text }</Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+  </Comment>
+);
+
 const MessageContainer = ({ data: { loading, messages } }) => (loading ? null : (
   <Messages>
-    {JSON.stringify(messages)}
+    <Comment.Group>
+      {messages.map(m => Message(m))}
+    </Comment.Group>
   </Messages>
 ));
 
@@ -18,7 +39,7 @@ const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
