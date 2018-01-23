4f824620e8b05740927f5bd412afdbb58e595622
kyle <kdeak1210@gmail.com>
2018-01-23 09:35:34 -0500

    Added the newChannelMessage Subscription
    Refactored the MessageContainer into a class and added the newChannelMessageSubscription. The component subscribes to this subscription in componentWillMount. Now, messages posted into a channel should display and update in real-time across users. The underlying tech is websockets...

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Comment } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Messages from '../components/Messages';
 
-const MessageContainer = ({ data: { loading, messages } }) => (loading ? null : (
-  <Messages>
-    <Comment.Group>
-      {messages.map(m => (
-        <Comment key={`message-${m.id}`}>
-          <Comment.Avatar src="/assets/images/avatar/small/matt.jpg" />
-          <Comment.Content>
-            <Comment.Author as="a">{ m.user.username }</Comment.Author>
-            <Comment.Metadata>
-              <div>{ m.created_at }</div>
-            </Comment.Metadata>
-            <Comment.Text>{ m.text }</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      ))}
-    </Comment.Group>
-  </Messages>
-));
+const newChannelMessageSubscription = gql`
+  subscription ($channelId: Int!) {
+    newChannelMessage(channelId: $channelId) {
+      id
+      text
+      user {
+        username
+      }
+      created_at
+    }
+  }
+`;
+
+class MessageContainer extends Component {
+  componentWillMount(){
+    this.props.data.subscribeToMore({
+      document: newChannelMessageSubscription,
+      variables: {
+        channelId: this.props.channelId,
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) {
+          return prev;
+        }
+        return {
+          ...prev,
+          messages: [...prev.messages, subscriptionData.data.newChannelMessage]
+        };
+      },
+    });
+  }
+
+  render(){
+    const { data: { messages, loading }} = this.props;
+    return loading ? null : (
+      <Messages>
+        <Comment.Group>
+          {messages.map(m => (
+            <Comment key={`message-${m.id}`}>
+              <Comment.Avatar src="/assets/images/avatar/small/matt.jpg" />
+              <Comment.Content>
+                <Comment.Author as="a">{ m.user.username }</Comment.Author>
+                <Comment.Metadata>
+                  <div>{ m.created_at }</div>
+                </Comment.Metadata>
+                <Comment.Text>{ m.text }</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </Messages>
+    )
+  }
+}
 
 const messagesQuery = gql`
   query ($channelId: Int!) {
